31274eca77ac5b29b4eca68dd221fed8332c8f83
Samir Patnaik <samir@petovera.com>
2019-09-24 17:55:30 +0530

    Updated the API URL to a constant variable

--- src/app/services/login-register.service.ts
+++ src/app/services/login-register.service.ts
@@ -5,38 +5,39 @@ import {Observable} from 'rxjs';
 import {LoginModel} from '../model/LoginModel';
 import {ProfileModel} from '../model/ProfileModel';
 
+const API_URL = 'http://54.81.168.191:8080/api/';
+
 @Injectable({
   providedIn: 'root'
 })
-
       
 export class LoginRegisterService {
 
   constructor(private _http: HttpClient, private session: SessionStorageService) { }
 
   login(body:any): Observable<LoginModel>{
-    return this._http.post<LoginModel>('http://54.81.168.191:8080/api/authenticate',body,{
+    return this._http.post<LoginModel>(API_URL+'authenticate',body,{
       observe:'body',
       headers:new HttpHeaders().append('Content-Type','application/json')
     });
   }
 
   register(body:any){
-    return this._http.post('http://54.81.168.191:8080/api/register',body,{
+    return this._http.post(API_URL+'register',body,{
       observe:'response',
       headers:new HttpHeaders().append('Content-Type','application/json')
     });
   }
 
   resetpwd(body:any){
-    return this._http.post('http://54.81.168.191:8080//api/account/reset-password/init',body,{
+    return this._http.post(API_URL+'account/reset-password/init',body,{
       observe:'response',
       headers:new HttpHeaders().append('Content-Type','application/json')
     });
   }
 
   fetchprofile(): Observable<ProfileModel>{
-    return this._http.get<ProfileModel>('http://54.81.168.191:8080/api/account',{
+    return this._http.get<ProfileModel>(API_URL+'account',{
       headers:new HttpHeaders({
         'Authorization': 'Bearer ' + this.session.get('userToken'),
         'Content-Type': 'application/json'
@@ -45,7 +46,7 @@ export class LoginRegisterService {
   }  
 
   updateprofile(body:any){
-    return this._http.post('http://54.81.168.191:8080/api/account',body,{
+    return this._http.post(API_URL+'account',body,{
       observe:'response',
       headers:new HttpHeaders({
         'Authorization': 'Bearer ' + this.session.get('userToken'),
@@ -55,7 +56,7 @@ export class LoginRegisterService {
   }
 
   changepassword(body:any){
-    return this._http.post('http://54.81.168.191:8080/api/account/change-password',body,{
+    return this._http.post(API_URL+'account/change-password',body,{
       observe:'response',
       headers:new HttpHeaders({
         'Authorization': 'Bearer ' + this.session.get('userToken'),
