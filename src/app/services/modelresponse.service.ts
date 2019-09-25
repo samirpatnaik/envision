eb8339dcd16d46a0ee9ce26930e04bcaa208cf4a
Samir Patnaik <samir@petovera.com>
2019-09-25 07:32:43 +0530

    New API has been updated

--- src/app/services/modelresponse.service.ts
+++ src/app/services/modelresponse.service.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { SessionStorageService } from 'angular-web-storage';
 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModelresponseService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private session: SessionStorageService) { }
 
   public apiData = new BehaviorSubject<any>(null);
   public apiData$ = this.apiData.asObservable();
@@ -21,10 +21,22 @@ export class ModelresponseService {
     return this.http.get("assets/modelresponse/ModelExecResponse.json");
   }
   */
+ private httpOptions = {
+  headers:new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+  };
 
 
   public submitModel(lid: number, body:any){
-    return this.http.post('http://13.58.79.119/eep/v1/model/'+`${lid}`,body,httpOptions) .pipe(map((data) => {
+    console.log(this.session.get('userToken'));
+    return this.http.post('http://54.81.168.191:8080/eep/v1/model/'+`${lid}`,body,
+      {
+        headers:new HttpHeaders({
+          'Authorization': 'Bearer ' + this.session.get('userToken'),
+          'Content-Type': 'application/json'
+        })
+      }) .pipe(map((data) => {
       return data;
     }));
  }
@@ -36,10 +48,10 @@ export class ModelresponseService {
   }
 
   public getModel(): Observable<any> {
-    return this.http.get('http://13.58.79.119/eep/v1/model', httpOptions);
+    return this.http.get('http://13.58.79.119/eep/v1/model', this.httpOptions);
   }
 
   public getModelFields(lid: number): Observable<any>{
-     return this.http.get('http://13.58.79.119/eep/v1/model/'+`${lid}`, httpOptions);
+     return this.http.get('http://13.58.79.119/eep/v1/model/'+`${lid}`, this.httpOptions);
   }
 }
